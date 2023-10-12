021c03033d8b36830d539d170cfed5a6ad9f3308
Krushnai jadhav <120713013+Krushnai2810@users.noreply.github.com>
2023-10-12 07:47:57 +0530

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,6 @@
+public class Block {
+	public static void main(String args[]) {
+		System.out.println("in main 1st line");
+	}
+
+}
---
+++ Block1.java
@@ -0,0 +1,13 @@
+public class Block1 {
+	{
+		System.out.println("In Instance blocks");
+	}
+	
+	static {
+		System.out.println("In static blocks ");
+	}
+	public static void main(String args[]) {
+		System.out.println("in main 1st line");
+	}
+
+}
